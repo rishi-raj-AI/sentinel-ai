8cdb6dbaf6ae124a6e42836f53a534df01b50c4d
rishi-raj-AI <rishirajgunjal333@gmail.com>
2026-08-13 18:46:26 +0100

    phase6: mount mission control globally

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import WorkspaceLayer from './workspace3'
+import MissionControl from './mission6'
 import './styles.css'
 import './ui2.css'
 import './workspace3a.css'
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <WorkspaceLayer />
+    <MissionControl />
   </React.StrictMode>,
 )
