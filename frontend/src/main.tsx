c2b377b223a114d7d44cd8d13623b6d21eb205f2
rishi-raj-AI <rishirajgunjal333@gmail.com>
2026-08-13 22:28:28 +0100

    Load active mission integration styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import './ui2.css'
 import './workspace3a.css'
 import './intelligenceWorkspace.css'
 import './reportStudio.css'
+import './missionActive.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
