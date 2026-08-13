24baf7fcbbeae4094acbe96a84a30f7c5a0e64b3
rishi-raj-AI <rishirajgunjal333@gmail.com>
2026-08-13 17:02:48 +0100

    Mount UI 3 workspace engine

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import WorkspaceLayer from './workspace3'
 import './styles.css'
 import './ui2.css'
+import './workspace3a.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <WorkspaceLayer />
   </React.StrictMode>,
 )
