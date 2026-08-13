c4cc4f5733dfdaccc54870224ec459bf8a793087
rishi-raj-AI <rishirajgunjal333@gmail.com>
2026-08-13 22:26:21 +0100

    Mount V2 intelligence workspace globally

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import WorkspaceLayer from './workspace3'
 import MissionControl from './mission6'
+import IntelligenceWorkspace from './intelligenceWorkspace'
 import './styles.css'
 import './ui2.css'
 import './workspace3a.css'
+import './intelligenceWorkspace.css'
+import './reportStudio.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <WorkspaceLayer />
     <MissionControl />
+    <IntelligenceWorkspace />
   </React.StrictMode>,
 )
