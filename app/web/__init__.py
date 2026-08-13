c004cb9a55c9cddb859916a9ad060e2dda046304
rishi-raj-AI <rishirajgunjal333@gmail.com>
2026-08-13 02:42:06 +0100

    phase34: mount enterprise APIs

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,6 +2,7 @@ from app.forensics.case_copilot_v6 import install_base_patch
 
 install_base_patch()
 
+from app.enterprise.api import install_enterprise_routes
 from app.web.autonomous import install_autonomous_routes
 from app.web.dashboard import DashboardService, create_dashboard_app as _create_dashboard_app
 
@@ -9,6 +10,7 @@ from app.web.dashboard import DashboardService, create_dashboard_app as _create_
 def create_dashboard_app(cases_root: str = "cases", sigma_rules: str = "rules/sigma"):
     app = _create_dashboard_app(cases_root=cases_root, sigma_rules=sigma_rules)
     install_autonomous_routes(app, cases_root=cases_root, sigma_rules=sigma_rules)
+    install_enterprise_routes(app, cases_root=cases_root)
     return app
 
 
